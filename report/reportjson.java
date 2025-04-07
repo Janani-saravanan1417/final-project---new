57811a507de3af742a9f02627b1a7812141eb580
Janani-saravanan1417 <jananisaravanan1417@gmail.com>
2025-04-08 03:00:11 +0530

    reportjson.java

---
+++ report/reportjson.java
@@ -0,0 +1,543 @@
+[
+  {
+    "line": 1,
+    "elements": [
+      {
+        "line": 2,
+        "name": "",
+        "description": "",
+        "type": "background",
+        "keyword": "Background",
+        "steps": [
+          {
+            "result": {
+              "duration": 10130962600,
+              "status": "passed"
+            },
+            "line": 3,
+            "name": "User should launch the Adactin Hotel Web Application Url",
+            "match": {
+              "location": "StepDefinition.userShouldLaunchTheAdactinHotelWebApplicationUrl()"
+            },
+            "keyword": "Given "
+          }
+        ]
+      },
+      {
+        "start_timestamp": "2024-03-01T06:08:10.585Z",
+        "line": 5,
+        "name": "validate login page with valid credentials",
+        "description": "",
+        "id": "validation-of-adactin-hotel-web-application-of-order-id-generation;validate-login-page-with-valid-credentials",
+        "type": "scenario",
+        "keyword": "Scenario",
+        "steps": [
+          {
+            "result": {
+              "duration": 552863200,
+              "status": "passed"
+            },
+            "line": 7,
+            "name": "User should enter valid username and password",
+            "match": {
+              "location": "StepDefinition.userShouldEnterValidUsernameAndPassword()"
+            },
+            "keyword": "When "
+          },
+          {
+            "result": {
+              "duration": 4876150500,
+              "status": "passed"
+            },
+            "line": 8,
+            "name": "Click login Button",
+            "match": {
+              "location": "StepDefinition.clickLoginButton()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 223631800,
+              "status": "passed"
+            },
+            "line": 13,
+            "name": "User should select location from dropdown",
+            "match": {
+              "location": "StepDefinition.userShouldSelectLocationFromDropdown()"
+            },
+            "keyword": "When "
+          },
+          {
+            "result": {
+              "duration": 426894400,
+              "status": "passed"
+            },
+            "line": 14,
+            "name": "User should select Hotel from dropdown",
+            "match": {
+              "location": "StepDefinition.userShouldSelectHotelFromDropdown()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 224208600,
+              "status": "passed"
+            },
+            "line": 15,
+            "name": "User should select Room Type from dropdown",
+            "match": {
+              "location": "StepDefinition.userShouldSelectRoomTypeFromDropdown()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 187963400,
+              "status": "passed"
+            },
+            "line": 16,
+            "name": "User should select No of Rooms from dropdown",
+            "match": {
+              "location": "StepDefinition.userShouldSelectNoOfRoomsFromDropdown()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 132693300,
+              "status": "passed"
+            },
+            "line": 17,
+            "name": "User should Enter Checkin Date as per the Format mentioned",
+            "match": {
+              "location": "StepDefinition.userShouldEnterCheckinDateAsPerTheFormatMentioned()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 150352200,
+              "status": "passed"
+            },
+            "line": 18,
+            "name": "User should Enter Checkout Date as per the Format mentioned",
+            "match": {
+              "location": "StepDefinition.userShouldEnterCheckoutDateAsPerTheFormatMentioned()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 185943200,
+              "status": "passed"
+            },
+            "line": 19,
+            "name": "User should select Adults per Room from dropdown",
+            "match": {
+              "location": "StepDefinition.userShouldSelectAdultsPerRoomFromDropdown()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 181364200,
+              "status": "passed"
+            },
+            "line": 20,
+            "name": "User should select Children per Room from dropdown",
+            "match": {
+              "location": "StepDefinition.userShouldSelectChildrenPerRoomFromDropdown()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 5033396800,
+              "status": "passed"
+            },
+            "line": 21,
+            "name": "click Search button",
+            "match": {
+              "location": "StepDefinition.clickSearchButton()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 114745700,
+              "status": "passed"
+            },
+            "line": 25,
+            "name": "user should select the checklist",
+            "match": {
+              "location": "StepDefinition.userShouldSelectTheChecklist()"
+            },
+            "keyword": "When "
+          },
+          {
+            "result": {
+              "duration": 4986203400,
+              "status": "passed"
+            },
+            "line": 26,
+            "name": "click Continue button",
+            "match": {
+              "location": "StepDefinition.clickContinueButton()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 171206200,
+              "status": "passed"
+            },
+            "line": 28,
+            "name": "User should Enter First Name in the TextBox",
+            "match": {
+              "location": "StepDefinition.userShouldEnterFirstNameInTheTextBox()"
+            },
+            "keyword": "When "
+          },
+          {
+            "result": {
+              "duration": 152377500,
+              "status": "passed"
+            },
+            "line": 29,
+            "name": "User should Enter Last Name in the TextBox",
+            "match": {
+              "location": "StepDefinition.userShouldEnterLastNameInTheTextBox()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 169363500,
+              "status": "passed"
+            },
+            "line": 30,
+            "name": "User should Enter Billing Address in the TextBox",
+            "match": {
+              "location": "StepDefinition.userShouldEnterBillingAddressInTheTextBox()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 156865000,
+              "status": "passed"
+            },
+            "line": 31,
+            "name": "User should Enter Credit Card No as per the Format mentioned in the TextBox",
+            "match": {
+              "location": "StepDefinition.userShouldEnterCreditCardNoAsPerTheFormatMentionedInTheTextBox()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 197973000,
+              "status": "passed"
+            },
+            "line": 32,
+            "name": "User should select Credit Card Type from dropdown",
+            "match": {
+              "location": "StepDefinition.userShouldSelectCreditCardTypeFromDropdown()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 383078700,
+              "status": "passed"
+            },
+            "line": 33,
+            "name": "User should select  Month and Year from Expiry Date dropdown",
+            "match": {
+              "location": "StepDefinition.userShouldSelectMonthAndYearFromExpiryDateDropdown()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 145203300,
+              "status": "passed"
+            },
+            "line": 34,
+            "name": "User should Enter CVV Number in the CVV Number TextBox",
+            "match": {
+              "location": "StepDefinition.userShouldEnterCVVNumberInTheCVVNumberTextBox()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 10121452300,
+              "status": "passed"
+            },
+            "line": 35,
+            "name": "click BookNow button",
+            "match": {
+              "location": "StepDefinition.clickBookNowButton()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 105695300,
+              "status": "passed"
+            },
+            "line": 36,
+            "name": "verify and Order id should be displayed to the User",
+            "match": {
+              "location": "StepDefinition.verifyAndOrderIdShouldBeDisplayedToTheUser()"
+            },
+            "keyword": "Then "
+          }
+        ]
+      },
+      {
+        "line": 2,
+        "name": "",
+        "description": "",
+        "type": "background",
+        "keyword": "Background",
+        "steps": [
+          {
+            "result": {
+              "duration": 5433373700,
+              "status": "passed"
+            },
+            "line": 3,
+            "name": "User should launch the Adactin Hotel Web Application Url",
+            "match": {
+              "location": "StepDefinition.userShouldLaunchTheAdactinHotelWebApplicationUrl()"
+            },
+            "keyword": "Given "
+          }
+        ]
+      },
+      {
+        "start_timestamp": "2024-03-01T06:08:49.724Z",
+        "line": 44,
+        "name": "validation login page with valid and invalid credentials",
+        "description": "",
+        "id": "validation-of-adactin-hotel-web-application-of-order-id-generation;validation-login-page-with-valid-and-invalid-credentials;;2",
+        "type": "scenario",
+        "keyword": "Scenario Outline",
+        "steps": [
+          {
+            "result": {
+              "duration": 742994700,
+              "status": "passed"
+            },
+            "line": 39,
+            "name": "User should enter valid \"Harsha73\" and \"Harish123@123\"",
+            "match": {
+              "arguments": [
+                {
+                  "val": "\"Harsha73\"",
+                  "offset": 24
+                },
+                {
+                  "val": "\"Harish123@123\"",
+                  "offset": 39
+                }
+              ],
+              "location": "StepDefinition.userShouldEnterValidAnd(String,String)"
+            },
+            "keyword": "When "
+          },
+          {
+            "result": {
+              "duration": 4499232400,
+              "status": "passed"
+            },
+            "line": 40,
+            "name": "Click login Button",
+            "match": {
+              "location": "StepDefinition.clickLoginButton()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 164176400,
+              "status": "passed"
+            },
+            "line": 41,
+            "name": "user able view error message",
+            "match": {
+              "location": "StepDefinition.userAbleViewErrorMessage()"
+            },
+            "keyword": "Then "
+          }
+        ]
+      },
+      {
+        "line": 2,
+        "name": "",
+        "description": "",
+        "type": "background",
+        "keyword": "Background",
+        "steps": [
+          {
+            "result": {
+              "duration": 5550700400,
+              "status": "passed"
+            },
+            "line": 3,
+            "name": "User should launch the Adactin Hotel Web Application Url",
+            "match": {
+              "location": "StepDefinition.userShouldLaunchTheAdactinHotelWebApplicationUrl()"
+            },
+            "keyword": "Given "
+          }
+        ]
+      },
+      {
+        "start_timestamp": "2024-03-01T06:09:00.580Z",
+        "line": 45,
+        "name": "validation login page with valid and invalid credentials",
+        "description": "",
+        "id": "validation-of-adactin-hotel-web-application-of-order-id-generation;validation-login-page-with-valid-and-invalid-credentials;;3",
+        "type": "scenario",
+        "keyword": "Scenario Outline",
+        "steps": [
+          {
+            "result": {
+              "duration": 562638700,
+              "status": "passed"
+            },
+            "line": 39,
+            "name": "User should enter valid \"Harsha73\" and \"Harish123\"",
+            "match": {
+              "arguments": [
+                {
+                  "val": "\"Harsha73\"",
+                  "offset": 24
+                },
+                {
+                  "val": "\"Harish123\"",
+                  "offset": 39
+                }
+              ],
+              "location": "StepDefinition.userShouldEnterValidAnd(String,String)"
+            },
+            "keyword": "When "
+          },
+          {
+            "result": {
+              "duration": 4477428600,
+              "status": "passed"
+            },
+            "line": 40,
+            "name": "Click login Button",
+            "match": {
+              "location": "StepDefinition.clickLoginButton()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 119440800,
+              "status": "passed"
+            },
+            "line": 41,
+            "name": "user able view error message",
+            "match": {
+              "location": "StepDefinition.userAbleViewErrorMessage()"
+            },
+            "keyword": "Then "
+          }
+        ]
+      },
+      {
+        "line": 2,
+        "name": "",
+        "description": "",
+        "type": "background",
+        "keyword": "Background",
+        "steps": [
+          {
+            "result": {
+              "duration": 6119080000,
+              "status": "passed"
+            },
+            "line": 3,
+            "name": "User should launch the Adactin Hotel Web Application Url",
+            "match": {
+              "location": "StepDefinition.userShouldLaunchTheAdactinHotelWebApplicationUrl()"
+            },
+            "keyword": "Given "
+          }
+        ]
+      },
+      {
+        "start_timestamp": "2024-03-01T06:09:11.299Z",
+        "line": 46,
+        "name": "validation login page with valid and invalid credentials",
+        "description": "",
+        "id": "validation-of-adactin-hotel-web-application-of-order-id-generation;validation-login-page-with-valid-and-invalid-credentials;;4",
+        "type": "scenario",
+        "keyword": "Scenario Outline",
+        "steps": [
+          {
+            "result": {
+              "duration": 461260100,
+              "status": "passed"
+            },
+            "line": 39,
+            "name": "User should enter valid \"Harish123\" and \"Harsha73\"",
+            "match": {
+              "arguments": [
+                {
+                  "val": "\"Harish123\"",
+                  "offset": 24
+                },
+                {
+                  "val": "\"Harsha73\"",
+                  "offset": 40
+                }
+              ],
+              "location": "StepDefinition.userShouldEnterValidAnd(String,String)"
+            },
+            "keyword": "When "
+          },
+          {
+            "result": {
+              "duration": 4427537200,
+              "status": "passed"
+            },
+            "line": 40,
+            "name": "Click login Button",
+            "match": {
+              "location": "StepDefinition.clickLoginButton()"
+            },
+            "keyword": "And "
+          },
+          {
+            "result": {
+              "duration": 162658200,
+              "status": "passed"
+            },
+            "line": 41,
+            "name": "user able view error message",
+            "match": {
+              "location": "StepDefinition.userAbleViewErrorMessage()"
+            },
+            "keyword": "Then "
+          }
+        ]
+      }
+    ],
+    "name": "validation of Adactin Hotel Web Application of Order Id Generation",
+    "description": "",
+    "id": "validation-of-adactin-hotel-web-application-of-order-id-generation",
+    "keyword": "Feature",
+    "uri": "file:src/test/resources/Features/adactin.feature",
+    "tags": []
+  }
+]
