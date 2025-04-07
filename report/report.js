5c78d4bc7af8e34fc7bb39db7cc350bc936f4935
Janani-saravanan1417 <jananisaravanan1417@gmail.com>
2025-04-08 03:08:05 +0530

    report.js

---
+++ report/report.js
@@ -0,0 +1,445 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/Features/adactin.feature");
+formatter.feature({
+  "name": "validation of Adactin Hotel Web Application of Order Id Generation",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User should launch the Adactin Hotel Web Application Url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldLaunchTheAdactinHotelWebApplicationUrl()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "validate login page with valid credentials",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "User should enter valid username and password",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldEnterValidUsernameAndPassword()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click login Button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.clickLoginButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should select location from dropdown",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldSelectLocationFromDropdown()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should select Hotel from dropdown",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldSelectHotelFromDropdown()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should select Room Type from dropdown",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldSelectRoomTypeFromDropdown()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should select No of Rooms from dropdown",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldSelectNoOfRoomsFromDropdown()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should Enter Checkin Date as per the Format mentioned",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldEnterCheckinDateAsPerTheFormatMentioned()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should Enter Checkout Date as per the Format mentioned",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldEnterCheckoutDateAsPerTheFormatMentioned()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should select Adults per Room from dropdown",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldSelectAdultsPerRoomFromDropdown()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should select Children per Room from dropdown",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldSelectChildrenPerRoomFromDropdown()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click Search button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.clickSearchButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user should select the checklist",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldSelectTheChecklist()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click Continue button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.clickContinueButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should Enter First Name in the TextBox",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldEnterFirstNameInTheTextBox()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should Enter Last Name in the TextBox",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldEnterLastNameInTheTextBox()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should Enter Billing Address in the TextBox",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldEnterBillingAddressInTheTextBox()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should Enter Credit Card No as per the Format mentioned in the TextBox",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldEnterCreditCardNoAsPerTheFormatMentionedInTheTextBox()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should select Credit Card Type from dropdown",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldSelectCreditCardTypeFromDropdown()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should select  Month and Year from Expiry Date dropdown",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldSelectMonthAndYearFromExpiryDateDropdown()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should Enter CVV Number in the CVV Number TextBox",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldEnterCVVNumberInTheCVVNumberTextBox()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click BookNow button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.clickBookNowButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "verify and Order id should be displayed to the User",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.verifyAndOrderIdShouldBeDisplayedToTheUser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenarioOutline({
+  "name": "validation login page with valid and invalid credentials",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "User should enter valid \"\u003cusername\u003e\" and \"\u003cpassword\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "Click login Button",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "user able view error message",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ]
+    },
+    {
+      "cells": [
+        "Harsha73",
+        "Harish123@123"
+      ]
+    },
+    {
+      "cells": [
+        "Harsha73",
+        "Harish123"
+      ]
+    },
+    {
+      "cells": [
+        "Harish123",
+        "Harsha73"
+      ]
+    }
+  ]
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User should launch the Adactin Hotel Web Application Url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldLaunchTheAdactinHotelWebApplicationUrl()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "validation login page with valid and invalid credentials",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "User should enter valid \"Harsha73\" and \"Harish123@123\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldEnterValidAnd(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click login Button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.clickLoginButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user able view error message",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.userAbleViewErrorMessage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User should launch the Adactin Hotel Web Application Url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldLaunchTheAdactinHotelWebApplicationUrl()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "validation login page with valid and invalid credentials",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "User should enter valid \"Harsha73\" and \"Harish123\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldEnterValidAnd(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click login Button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.clickLoginButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user able view error message",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.userAbleViewErrorMessage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "User should launch the Adactin Hotel Web Application Url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldLaunchTheAdactinHotelWebApplicationUrl()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "validation login page with valid and invalid credentials",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "User should enter valid \"Harish123\" and \"Harsha73\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.userShouldEnterValidAnd(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click login Button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.clickLoginButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user able view error message",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.userAbleViewErrorMessage()"
+});
+formatter.result({
+  "status": "passed"
+});
+});
